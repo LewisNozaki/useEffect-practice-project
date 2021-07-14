0d89b879dd4de4dc269b53c89309808b188459a5
Lewis Nozaki <lhnozaki@outlook.com>
2021-07-13 16:07:48 -1000

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import styles from './Home.module.css';
 
 const Home = (props) => {
   return (
-    <Card className={classes.home}>
+    <Card className={styles.home}>
       <h1>Welcome back!</h1>
     </Card>
   );
