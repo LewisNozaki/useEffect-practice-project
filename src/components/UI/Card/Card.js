6a2c16061395a06d882bc27ab86e64cb1795ae98
Lewis Nozaki <lhnozaki@outlook.com>
2021-07-13 16:11:20 -1000

    updates

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Card.module.css';
 
-import classes from './Card.module.css';
-
-const Card = (props) => {
+const Card = ({ className, children}) => {
   return (
-    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+    <div className={`${styles.card} ${className}`}>
+      {children}
+    </div>
   );
 };
 
